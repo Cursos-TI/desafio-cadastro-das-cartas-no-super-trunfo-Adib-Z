ab484f00b85ed5e5ce0475ec993f3fac3c1a8b5d
Adib-Z <adibzuriel280@gmail.com>
2025-03-13 18:13:21 +0000

    super trunfo

---
+++ .vscode/supertrunfo.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int populaçao, turisticos;
+    float area, PIB;
+    char estado[50], codigo[4], cidade[20];
+    
+
+    printf("Digite o nome do estado: \n");
+    scanf("%s", &estado);
+
+    printf("Digite o codigo da carta: \n");
+    scanf("%s", &codigo);
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", &cidade);
+    
+    printf("Digite a população: \n");
+    scanf("%d", &populaçao);
+
+    printf("Digite a area: \n");
+    scanf("%f", &area);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &PIB);
+
+    printf("Digite o numero de pontos turisticos: \n");
+    scanf("%d", &turisticos);
+
+   
+
+
+}
